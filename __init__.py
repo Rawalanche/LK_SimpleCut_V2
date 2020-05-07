999fd378a1e9de562d05bb618d4b0cf4968fe66c
rawalanche <rawalanche@gmail.com>
2020-05-07 17:06:26 +0200

    Basic cutting works

--- __init__.py
+++ __init__.py
@@ -55,12 +55,16 @@ class SimpleCut(bpy.types.Operator):
 
     def invoke(self, context, event):
 
-        if context.space_data.type == 'VIEW_3D':
-            context.window_manager.modal_handler_add(self)
-            return {'RUNNING_MODAL'}
-        else:
+        if len(context.selected_objects) == 0:
+            self.report({'WARNING'}, "No mesh object selected!")
+            return {'CANCELLED'}
+        elif context.space_data.type != 'VIEW_3D':
             self.report({'WARNING'}, "Active space must be a View3d")
             return {'CANCELLED'}
+        else:
+            self.selected_object = context.active_object
+            context.window_manager.modal_handler_add(self)
+            return {'RUNNING_MODAL'}
 
     def modal(self, context, event):
         context.area.tag_redraw()
@@ -97,48 +101,67 @@ def Cut(self, context):
     region = context.region
     rv3d = context.region_data
 
-    TopLeft = (self.mouse_path[0][0], self.mouse_path[0][1])
-    TopRight = (self.mouse_path[1][0], self.mouse_path[0][1])
-    BottomLeft = (self.mouse_path[0][0], self.mouse_path[1][1])
-    BottomRight = (self.mouse_path[1][0], self.mouse_path[1][1])
+    if len(self.mouse_path) > 0:
+        TopLeft = (self.mouse_path[0][0], self.mouse_path[0][1])
+        TopRight = (self.mouse_path[1][0], self.mouse_path[0][1])
+        BottomLeft = (self.mouse_path[0][0], self.mouse_path[1][1])
+        BottomRight = (self.mouse_path[1][0], self.mouse_path[1][1])
 
-    verts = (
+    vertices_2d = (
         TopLeft,
         TopRight,
         BottomRight,
         BottomLeft,
     )
 
-    # Get a point on a infinite plane and its direction
+    # Create a new empty bmesh
+    cutter_bmesh = bmesh.new()
+
     plane_normal = region_2d_to_vector_3d(region, rv3d, self.mouse_path[0])
-
     plane_location = Vector((0.0, 0.0, 0.0))
+    vertices_3d = []
 
-    faces = []
+    # Create faces from vertices and add them to the faces array
+    for vertex_2d in vertices_2d:
 
-    # New mesh
-    mesh = bpy.data.meshes.new('CMT_Square')
-    cut_bmesh = bmesh.new()
-    cut_bmesh.from_mesh(mesh)
+        vertex_direction = region_2d_to_vector_3d(region, rv3d, vertex_2d)
+        vertex_3d = region_2d_to_location_3d(region, rv3d, vertex_2d, vertex_direction)
 
-    # New object and link it to the scene
-    ob = bpy.data.objects.new('CMT_Square', mesh)
-    context.collection.objects.link(ob)
+        line_start = vertex_3d
+        line_end = vertex_3d + (plane_normal * 10000.0)
 
-    # Find the intersection of a line going thru each vertex and the infinite plane
-    for v_co in verts:
-        vec = region_2d_to_vector_3d(region, rv3d, v_co)
-        p0 = region_2d_to_location_3d(region, rv3d, v_co, vec)
-        p1 = region_2d_to_location_3d(region, rv3d, v_co, vec) + plane_normal * 10000.0
-        faces.append(cut_bmesh.verts.new(intersect_line_plane(p0, p1, plane_location, plane_normal)))
+        vertex_intersection = intersect_line_plane(line_start, line_end, plane_location, plane_normal)
+        vertices_3d.append(cutter_bmesh.verts.new(vertex_intersection))
 
     # Update vertices index
-    cut_bmesh.verts.index_update()
-    # New faces
-    t_face = cut_bmesh.faces.new(faces)
+    cutter_bmesh.verts.index_update()
 
-    # Set mesh
-    cut_bmesh.to_mesh(mesh)
+    # New faces
+    cutter_bmesh.faces.new(vertices_3d)
+
+    # Create new empty mesh
+    mesh = bpy.data.meshes.new('SM_Cutter')
+
+    # Add bmesh data to the created empty mesh
+    cutter_bmesh.to_mesh(mesh)
+
+    # Create new object from the mesh and add it to the active collection
+    cutter_object = bpy.data.objects.new('Cutter', mesh)
+    context.collection.objects.link(cutter_object)
+
+    # Add solidify modifier to cutter object and set its thickness
+    solidify_modifier = cutter_object.modifiers.new(type="SOLIDIFY", name="CutterSolidify")
+    solidify_modifier.thickness = 1.0
+
+    # Add boolean modifier to selected object and set cutter as boolean object
+    boolean_modifier = self.selected_object.modifiers.new(type="BOOLEAN", name="CutterBoolean")
+    boolean_modifier.object = cutter_object
+
+    # Apply boolean modifier to (currently active) selected object
+    bpy.ops.object.modifier_apply(apply_as='DATA', modifier="CutterBoolean")
+
+    # Delete cutter object
+    bpy.data.objects.remove(cutter_object)
 
 
 def DrawInViewport(self, context):
