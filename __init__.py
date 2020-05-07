a61c6b46f2db4e87489489a0ef70c6f00b72f41a
rawalanche <rawalanche@gmail.com>
2020-05-07 11:59:08 +0200

    Backup before cut mesh changes

--- __init__.py
+++ __init__.py
@@ -17,12 +17,25 @@
 # ##### END GPL LICENSE BLOCK #####
 
 import bpy
+from bpy.props import FloatVectorProperty
+from bpy_extras import view3d_utils
+from bpy_extras.view3d_utils import (
+    region_2d_to_vector_3d,
+    region_2d_to_location_3d,
+    location_3d_to_region_2d,
+)
+
 import bgl
 import blf
 import gpu
+import bmesh
 from gpu_extras.batch import batch_for_shader
+
 from mathutils import Vector
-from bpy.props import FloatVectorProperty
+from mathutils.geometry import (
+    intersect_line_plane,
+)
+
 
 bl_info = {
     "name": "SimpleCut V2",
@@ -60,10 +73,11 @@ class SimpleCut(bpy.types.Operator):
                 ]
 
                 args = (self, context)
-                self._handle = bpy.types.SpaceView3D.draw_handler_add(draw_callback_px, args, 'WINDOW', 'POST_PIXEL')
+                self.draw_handler = bpy.types.SpaceView3D.draw_handler_add(DrawInViewport, args, 'WINDOW', 'POST_PIXEL')
 
             elif event.value == 'RELEASE':
-                bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
+                Cut(self, context)
+                bpy.types.SpaceView3D.draw_handler_remove(self.draw_handler, 'WINDOW')
                 return {'FINISHED'}
 
         elif event.type == 'MOUSEMOVE':
@@ -71,13 +85,63 @@ class SimpleCut(bpy.types.Operator):
                 self.mouse_path[1] = (event.mouse_region_x, event.mouse_region_y)
 
         elif event.type in {'RIGHTMOUSE', 'ESC'}:
-            bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
+            bpy.types.SpaceView3D.draw_handler_remove(self.draw_handler, 'WINDOW')
             return {'CANCELLED'}
 
         return {'RUNNING_MODAL'}
 
 
-def draw_callback_px(self, context):
+def Cut(self, context):
+    """ Create a rectangle mesh """
+    # Scene information
+    region = context.region
+    rv3d = context.region_data
+
+    TopLeft = (self.mouse_path[0][0], self.mouse_path[0][1])
+    TopRight = (self.mouse_path[1][0], self.mouse_path[0][1])
+    BottomLeft = (self.mouse_path[0][0], self.mouse_path[1][1])
+    BottomRight = (self.mouse_path[1][0], self.mouse_path[1][1])
+
+    verts = (
+        TopLeft,
+        TopRight,
+        BottomRight,
+        BottomLeft,
+    )
+
+    # Get a point on a infinite plane and its direction
+    plane_normal = region_2d_to_vector_3d(region, rv3d, self.mouse_path[0])
+
+    plane_location = Vector((0.0, 0.0, 0.0))
+
+    faces = []
+
+    # New mesh
+    mesh = bpy.data.meshes.new('CMT_Square')
+    cut_bmesh = bmesh.new()
+    cut_bmesh.from_mesh(mesh)
+
+    # New object and link it to the scene
+    ob = bpy.data.objects.new('CMT_Square', mesh)
+    context.collection.objects.link(ob)
+
+    # Find the intersection of a line going thru each vertex and the infinite plane
+    for v_co in verts:
+        vec = region_2d_to_vector_3d(region, rv3d, v_co)
+        p0 = region_2d_to_location_3d(region, rv3d, v_co, vec)
+        p1 = region_2d_to_location_3d(region, rv3d, v_co, vec) + plane_normal * 10000.0
+        faces.append(cut_bmesh.verts.new(intersect_line_plane(p0, p1, plane_location, plane_normal)))
+
+    # Update vertices index
+    cut_bmesh.verts.index_update()
+    # New faces
+    t_face = cut_bmesh.faces.new(faces)
+
+    # Set mesh
+    cut_bmesh.to_mesh(mesh)
+
+
+def DrawInViewport(self, context):
     bgl.glLineWidth(2)
     bgl.glEnable(bgl.GL_BLEND)
     bgl.glEnable(bgl.GL_LINE_SMOOTH)
@@ -91,18 +155,14 @@ def draw_callback_px(self, context):
     BottomLeft = (self.mouse_path[0][0], self.mouse_path[1][1])
     BottomRight = (self.mouse_path[1][0], self.mouse_path[1][1])
 
-    lines = (
+    verts = (
         TopLeft,
         TopRight,
+        BottomRight,
         BottomLeft,
-        BottomRight
     )
 
-    indices = (
-        (0, 1), (1, 3), (3, 2), (2, 0)
-    )
-
-    batch = batch_for_shader(shader, 'LINES', {"pos": lines}, indices=indices)
+    batch = batch_for_shader(shader, 'LINE_LOOP', {"pos": verts})
 
     batch.draw(shader)
 
