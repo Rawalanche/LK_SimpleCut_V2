7c03721556d197b21fd281b9fd81e6592c86c942
rawalanche <rawalanche@gmail.com>
2020-05-21 11:36:03 +0200

    Cleaned up cutting modes

--- __init__.py
+++ __init__.py
@@ -36,10 +36,11 @@ from mathutils.geometry import (
     intersect_line_plane,
 )
 
+import math
 
 bl_info = {
     "name": "SimpleCut V2",
-    "author": "Ludvik Koutny, Pixivore, Cedric LEPILLER, Ted Milker, Clarkx",
+    "author": "Ludvik Koutny",
     "description": "A simple tool to cut meshes using boolean operations",
     "version": (1, 2, 0),
     "blender": (2, 80, 0),
@@ -58,151 +59,230 @@ class SimpleCut(bpy.types.Operator):
         if len(context.selected_objects) == 0:
             self.report({'WARNING'}, "No mesh object selected!")
             return {'CANCELLED'}
+
         elif context.space_data.type != 'VIEW_3D':
             self.report({'WARNING'}, "Active space must be a View3d")
             return {'CANCELLED'}
+
         else:
             self.selected_object = context.active_object
+            self.editing = False
+            self.shape = 'RECTANGLE'
             context.window_manager.modal_handler_add(self)
+            bpy.context.window.cursor_set("CROSSHAIR")
             return {'RUNNING_MODAL'}
 
     def modal(self, context, event):
         context.area.tag_redraw()
 
+        # LMB behavior
         if event.type == 'LEFTMOUSE':
+
+            # Press
             if event.value == 'PRESS':
-                self.mouse_path = [
-                    (event.mouse_region_x, event.mouse_region_y),
-                    (event.mouse_region_x, event.mouse_region_y)
-                ]
+                if self.editing is False:
+                    self.editing = True
 
-                args = (self, context)
-                self.draw_handler = bpy.types.SpaceView3D.draw_handler_add(DrawInViewport, args, 'WINDOW', 'POST_PIXEL')
+                    self.mouse_path = [
+                        (event.mouse_region_x, event.mouse_region_y),
+                        (event.mouse_region_x, event.mouse_region_y)
+                    ]
 
-            elif event.value == 'RELEASE':
-                Cut(self, context)
-                bpy.types.SpaceView3D.draw_handler_remove(self.draw_handler, 'WINDOW')
-                return {'FINISHED'}
+                    args = (self.shape, self.mouse_path)
+                    self.draw_handler = bpy.types.SpaceView3D.draw_handler_add(
+                        self.draw_in_viewport, args, 'WINDOW', 'POST_PIXEL')
 
+            # Release
+            elif event.value == 'RELEASE' and self.editing is True:
+                if self.shape is 'POLYGON':
+                    # Compare distance from current mouse position to initial shape point
+                    initial_point = Vector((self.mouse_path[0]))
+                    current_point = Vector((event.mouse_region_x, event.mouse_region_y))
+                    initial_point_distance = (current_point - initial_point).length
+
+                    # Perform cut if we are close to initial point and have at least 3 points (triangle)
+                    if initial_point_distance < 10 and len(self.mouse_path) > 2:
+                        self.editing = False
+                        self.mouse_path.pop()  # Removes last point from the mouse path
+                        self.create_cutter_object(context, self.shape, self.mouse_path)
+                        bpy.types.SpaceView3D.draw_handler_remove(self.draw_handler, 'WINDOW')
+
+                    # Otherwise add one more point
+                    else:
+                        self.mouse_path.append((event.mouse_region_x, event.mouse_region_y))
+
+                else:
+                    self.editing = False
+                    self.create_cutter_object(context, self.shape, self.mouse_path)
+                    bpy.types.SpaceView3D.draw_handler_remove(self.draw_handler, 'WINDOW')
+
+                return {'RUNNING_MODAL'}
+
+        # Mouse move behavior
         elif event.type == 'MOUSEMOVE':
             if hasattr(self, 'mouse_path'):
-                self.mouse_path[1] = (event.mouse_region_x, event.mouse_region_y)
+                self.mouse_path[len(self.mouse_path) - 1] = (event.mouse_region_x, event.mouse_region_y)
 
-        elif event.type in {'RIGHTMOUSE', 'ESC'}:
-            bpy.types.SpaceView3D.draw_handler_remove(self.draw_handler, 'WINDOW')
+        # RMB behavior
+        elif event.type == 'RIGHTMOUSE' and event.value == 'PRESS':
+            if hasattr(self, 'draw_handler') and self.editing is True:
+                bpy.types.SpaceView3D.draw_handler_remove(self.draw_handler, 'WINDOW')
             return {'CANCELLED'}
+
+        # TAB behavior
+        elif event.type == 'TAB' and event.value == 'PRESS' and self.editing is False:
+            if self.shape == 'RECTANGLE':
+                self.shape = 'POLYGON'
+            elif self.shape == 'POLYGON':
+                self.shape = 'CIRCLE'
+            elif self.shape == 'CIRCLE':
+                self.shape = 'RECTANGLE'
+
+        else:
+            return {'PASS_THROUGH'}
 
         return {'RUNNING_MODAL'}
 
+    def get_2d_shape(self, shape, mouse_path):
+        vertices = []
 
-def Cut(self, context):
-    """ Create a rectangle mesh """
-    # Scene information
-    region = context.region
-    rv3d = context.region_data
-    sel_obj = self.selected_object
+        # Create 2D rectangle shape
+        if shape is 'RECTANGLE':
+            # Set OpenGL drawing mode to Triangle Strip
+            draw_mode = 'LINE_LOOP'
 
-    if len(self.mouse_path) > 0:
-        TopLeft = (self.mouse_path[0][0], self.mouse_path[0][1])
-        TopRight = (self.mouse_path[1][0], self.mouse_path[0][1])
-        BottomLeft = (self.mouse_path[0][0], self.mouse_path[1][1])
-        BottomRight = (self.mouse_path[1][0], self.mouse_path[1][1])
+            # Translate start and end of the mouse click to triangle corner coordinates
+            TopLeft = (mouse_path[0][0], mouse_path[0][1])
+            TopRight = (mouse_path[1][0], mouse_path[0][1])
+            BottomLeft = (mouse_path[0][0], mouse_path[1][1])
+            BottomRight = (mouse_path[1][0], mouse_path[1][1])
 
-    vertices_2d = (
-        TopLeft,
-        TopRight,
-        BottomRight,
-        BottomLeft,
-    )
+            vertices = [TopLeft, TopRight, BottomRight, BottomLeft]
 
-    # Calculate bbox center of selected object. Multipled by 1/8 because bound_box returns array of 8 vectors.
-    local_bbox_center = 0.125 * sum((Vector(b) for b in sel_obj.bound_box), Vector())
+        # Create 2D polygon
+        elif shape is 'POLYGON':
+            # Set OpenGL drawing mode to Triangle Strip
+            draw_mode = 'LINE_LOOP'
 
-    # Convert bbox from local to world space
-    bbox_center = sel_obj.matrix_world @ local_bbox_center
+            vertices = mouse_path
 
-    # Location and normal for the intersection plan
-    plane_location = bbox_center
-    plane_normal = region_2d_to_vector_3d(region, rv3d, self.mouse_path[0])
+        # Create 2D circle shape
+        elif shape is 'CIRCLE':
+            # Set OpenGL drawing mode to Triangle Fan
+            draw_mode = 'LINE_LOOP'
 
-    # Create a new empty bmesh
-    cutter_bmesh = bmesh.new()
+            # Length from the start and end of the mouse click to be used as circle radius
+            mouse_start = Vector((mouse_path[0]))
+            mouse_end = Vector((mouse_path[1]))
+            radius = (mouse_end - mouse_start).length
 
-    vertices_3d = []
+            # Set origin of the circle to be at the mouse click start location
+            origin = mouse_path[0]
 
-    # Create faces from vertices and add them to the faces array
-    for vertex_2d in vertices_2d:
+            # Create triangle fan circling around center vertice
+            for angle in range(0, 360, 12):
+                vert = (
+                    origin[0] + (math.cos(math.radians(angle)) * radius),
+                    origin[1] + (math.sin(math.radians(angle)) * radius))
+                vertices.append(vert)
 
-        vertex_direction = region_2d_to_vector_3d(region, rv3d, vertex_2d)
-        vertex_3d = region_2d_to_location_3d(region, rv3d, vertex_2d, vertex_direction)
+        return (draw_mode, vertices)
 
-        line_start = vertex_3d
-        line_end = vertex_3d + (plane_normal * 10000.0)
+    def draw_in_viewport(self, shape, mouse_path):
 
-        vertex_intersection = intersect_line_plane(line_start, line_end, plane_location, plane_normal)
-        vertices_3d.append(cutter_bmesh.verts.new(vertex_intersection))
+        # Set OpenGL settings
+        bgl.glLineWidth(3)
+        bgl.glEnable(bgl.GL_BLEND)
+        bgl.glEnable(bgl.GL_LINE_SMOOTH)
 
-    # Update vertices index
-    cutter_bmesh.verts.index_update()
+        # Create OpenGL shader
+        shader = gpu.shader.from_builtin('2D_UNIFORM_COLOR')
+        shader.bind()
+        shader.uniform_float("color", (0.8, 0.1, 0.1, 0.5))
 
-    # New faces
-    cutter_bmesh.faces.new(vertices_3d)
+        # Get OpenGL draw mode and vertices to draw
+        draw_mode, vertices = self.get_2d_shape(shape, mouse_path)
 
-    # Create new empty mesh
-    mesh = bpy.data.meshes.new('SM_Cutter')
+        # Make a shader batch and draw it
+        batch = batch_for_shader(shader, draw_mode, {"pos": vertices})
+        batch.draw(shader)
 
-    # Add bmesh data to the created empty mesh
-    cutter_bmesh.to_mesh(mesh)
+        # Restore OpenGL settings
+        bgl.glLineWidth(1)
+        bgl.glDisable(bgl.GL_BLEND)
+        bgl.glDisable(bgl.GL_LINE_SMOOTH)
 
-    # Create new object from the mesh and add it to the active collection
-    cutter_object = bpy.data.objects.new('Cutter', mesh)
-    context.collection.objects.link(cutter_object)
+    def create_cutter_object(self, context, shape, mouse_path):
+        """ Create a rectangle mesh """
+        # Scene information
+        region = context.region
+        rv3d = context.region_data
+        sel_obj = self.selected_object
 
-    # Add solidify modifier to cutter object and set its thickness
-    solidify_modifier = cutter_object.modifiers.new(type="SOLIDIFY", name="CutterSolidify")
-    solidify_modifier.thickness = sum(sel_obj.dimensions)
-    solidify_modifier.offset = 0.0
+        vertices_2d = self.get_2d_shape(shape, mouse_path)[1]
 
-    # Add boolean modifier to selected object and set cutter as boolean object
-    boolean_modifier = sel_obj.modifiers.new(type="BOOLEAN", name="CutterBoolean")
-    boolean_modifier.object = cutter_object
+        # Calculate bbox center of selected object. Multipled by 1/8 because bound_box returns array of 8 vectors.
+        local_bbox_center = 0.125 * sum((Vector(b) for b in sel_obj.bound_box), Vector())
 
-    # Apply boolean modifier to (currently active) selected object
-    bpy.ops.object.modifier_apply(apply_as='DATA', modifier="CutterBoolean")
+        # Convert bbox from local to world space
+        bbox_center = sel_obj.matrix_world @ local_bbox_center
 
-    # Delete cutter object
-    bpy.data.objects.remove(cutter_object)
+        # Location and normal for the intersection plan
+        plane_location = bbox_center
+        plane_normal = region_2d_to_vector_3d(region, rv3d, self.mouse_path[0])
 
+        # Create a new empty bmesh
+        cutter_bmesh = bmesh.new()
 
-def DrawInViewport(self, context):
-    bgl.glLineWidth(2)
-    bgl.glEnable(bgl.GL_BLEND)
-    bgl.glEnable(bgl.GL_LINE_SMOOTH)
+        vertices_3d = []
 
-    shader = gpu.shader.from_builtin('2D_UNIFORM_COLOR')
-    shader.bind()
-    shader.uniform_float("color", (0.8, 0.1, 0.1, 0.5))
+        # Create faces from vertices and add them to the faces array
+        for vertex_2d in vertices_2d:
 
-    TopLeft = (self.mouse_path[0][0], self.mouse_path[0][1])
-    TopRight = (self.mouse_path[1][0], self.mouse_path[0][1])
-    BottomLeft = (self.mouse_path[0][0], self.mouse_path[1][1])
-    BottomRight = (self.mouse_path[1][0], self.mouse_path[1][1])
+            vertex_direction = region_2d_to_vector_3d(region, rv3d, vertex_2d)
+            vertex_3d = region_2d_to_location_3d(region, rv3d, vertex_2d, vertex_direction)
 
-    verts = (
-        TopLeft,
-        TopRight,
-        BottomRight,
-        BottomLeft,
-    )
+            line_start = vertex_3d
+            line_end = vertex_3d + (plane_normal * 10000.0)  # TODO remove magic number
 
-    batch = batch_for_shader(shader, 'LINE_LOOP', {"pos": verts})
+            vertex_intersection = intersect_line_plane(line_start, line_end, plane_location, plane_normal)
+            vertices_3d.append(cutter_bmesh.verts.new(vertex_intersection))
 
-    batch.draw(shader)
+        # Update vertices index
+        cutter_bmesh.verts.index_update()
 
-    # restore opengl defaults
-    bgl.glLineWidth(1)
-    bgl.glDisable(bgl.GL_BLEND)
-    bgl.glDisable(bgl.GL_LINE_SMOOTH)
+        # New faces
+        cutter_bmesh.faces.new(vertices_3d)
+
+        # Create new empty mesh
+        mesh = bpy.data.meshes.new('SM_Cutter')
+
+        # Add bmesh data to the created empty mesh
+        cutter_bmesh.to_mesh(mesh)
+
+        # Create new object from the mesh and add it to the active collection
+        cutter_object = bpy.data.objects.new('Cutter', mesh)
+        context.collection.objects.link(cutter_object)
+
+        # Add solidify modifier to cutter object and set its thickness
+        solidify_modifier = cutter_object.modifiers.new(type="SOLIDIFY", name="CutterSolidify")
+        solidify_modifier.thickness = sum(sel_obj.dimensions)
+        solidify_modifier.offset = 0.0
+
+        self.cut_selected_object(cutter_object)
+
+    def cut_selected_object(self, cutter_object):
+        sel_obj = self.selected_object
+
+        # Add boolean modifier to selected object and set cutter as boolean object
+        boolean_modifier = sel_obj.modifiers.new(type="BOOLEAN", name="CutterBoolean")
+        boolean_modifier.object = cutter_object
+
+        # Apply boolean modifier to (currently active) selected object
+        bpy.ops.object.modifier_apply(apply_as='DATA', modifier="CutterBoolean")
+
+        # Delete cutter object
+        bpy.data.objects.remove(cutter_object)
 
 
 addon_keymaps = []
